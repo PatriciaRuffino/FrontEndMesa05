b52739536f6518cbf1d358cbe10590e54ecac71b
PatriciaRuffino <sindelformr@gmail.com>
2021-10-27 19:30:09 -0300

    Atualizacao da descricao

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,8 +4,8 @@ const Footer = () => {
             <div className="container">
              <p className="text-light">App desenvolvido por Patricia, Anderson, Willian e Ronilson</p>
              <p className="text-light"> Contato Mesa 04</p>
-            <p className="text-light"><small><strong>Semana Spring React</strong><br/>
-      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            <p className="text-light"><small><strong>Mesa Integradora 05</strong><br/>
+      Evento promovido pelo prof. Renan</small></p>
             </div>
         </footer>
      
